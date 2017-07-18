53bf3377ab9059ae6fde86a333e1ceb95f94d525
Kevin Dantas <kevin.ds.29@gmail.com>
2017-07-18 16:36:21 -0300

    Updates

--- src/icons/index.js
+++
@@ -1,6 +0,0 @@
-import DateIcon from './DateIcon';
-import TimeIcon from './TimeIcon';
-import SeatIcon from './SeatIcon';
-import LocationIcon from './LocationIcon';
-
-export { DateIcon, TimeIcon, SeatIcon, LocationIcon };
