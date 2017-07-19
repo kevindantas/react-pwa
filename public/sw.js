3bceac1572881e4fb2373efee6fb4ee7707188c2
Kevin-ds <kevin.ds.29@gmail.com>
2017-07-19 03:12:50 -0300

    Change default SW

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', function(event) {
+//   event.waitUntil(self.skipWaiting());
+    event.waitUntil(
+        caches.open('cache-v1')
+            .then((cache) => {
+                cache.addAll([
+                    'index.html'
+                ]);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cache) => {
+                if (cache) return cache;
+
+                return fetch(event.request);
+            })
+    );
+});
+
+
+// self.addEventListener('fetch', function(event) {
+//     const { url } = event.request;
+//     if (url.endsWith('.svg')) {
+//         event.respondWith(
+//             fetch('http://i.imgur.com/mxs6D.gif')
+//         )
+//     }
+// });
+
+// self.addEventListener()
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,9 +19,9 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
+  const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -31,6 +31,22 @@ export default function register() {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (!isLocalhost) {
+        // Is not local host. Just register service worker
+        registerValidSW(swUrl);
+      } else {
+        // This is running on localhost. Lets check if a service worker still exists or not.
+        checkValidServiceWorker(swUrl);
+      }
+    });
+  } else {
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
