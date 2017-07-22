affa61a3c622967af1408792d292f2faf3e05115
Kevin-ds <kevin.ds.29@gmail.com>
2017-07-22 01:29:27 -0300

    NotificationButton close notification after 3000ms
    Add comments ftw

--- src/components/NotificationButton/index.js
+++ src/components/NotificationButton/index.js
@@ -4,10 +4,16 @@ import './style.css';
 const handleOnClick = () => {
     Notification.requestPermission()
         .then((permission) => {
+            // If the user granted permission
             if (permission === 'granted') {
-                new Notification('Tô vivão!');
+                // Show a notification
+                const notification = new Notification('Tô vivão!');
+                // Close notification after 3000ms
+                setTimeout(() => notification.close(), 3000);
             } else {
                 console.log('Não tô vivão :(');
+                // Since our user haven't granted permission,
+                // these notification will not appear
                 new Notification('Não tô vivão :(');
             }
         })
