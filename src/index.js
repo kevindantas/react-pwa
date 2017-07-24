6fa0001dad155c68c69cb9d57e33d54e8c16da7f
Kevin-ds <kevin.ds.29@gmail.com>
2017-07-23 21:24:05 -0300

    Add listener to beforeinstallpromt

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// Listen when the user would be prompted to add to homescreen
+window.addEventListener("beforeinstallprompt", function(e) { 
+  // Don't show the prompt the user to add to homescreen
+  e.preventDefault();
+
+  // Store the prompt event on a global variable
+  // So we can use window.deferredPrompt.prompt() when we really want
+  window.deferredPrompt = e;
+
+  // log the platforms provided as options in an install prompt 
+  console.log(e); // e.g., ["web", "android", "windows"] 
+  return false;
+});
