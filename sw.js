0d63dd2aebda2d824355ac920bc5279fc48ae997
Kevin-ds <kevin.ds.29@gmail.com>
2017-07-23 23:46:32 -0300

    Updates

---
+++ sw.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('cache-v1')
+            .then((cache) => {
+                cache.addAll([
+                    'index.html'
+                ]);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cache) => {
+                if (cache) return cache;
+
+                return fetch(event.request);
+            })
+    );
+});
+
+
+// self.addEventListener('fetch', function(event) {
+//     const { url } = event.request;
+//     if (url.endsWith('.svg')) {
+//         event.respondWith(
+//             fetch('http://i.imgur.com/mxs6D.gif')
+//         )
+//     }
+// });
+
+// self.addEventListener()
